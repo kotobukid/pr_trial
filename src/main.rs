84671073f1f3844b0c045dfa1533b7c74a0ce0ba
kotobukid <kotobuki@daqua.jp>
2024-05-16 13:55:31 +0900

    テストを追加 GithubActionsでテストされるか試す

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,22 @@ fn say_hello(name: &str) -> () {
     println!("Hello, {name}!")
 }
 
+pub fn add_u8(a: u8, b: u8) -> u8 {
+    a + b
+}
+
 fn main() {
     println!("Hello, world!");
 
     say_goodbye()
+}
+
+#[cfg(test)]
+mod sample_test {
+    use crate::*;
+
+    #[test]
+    fn test1() {
+        assert_eq!(8, add_u8(4, 4))
+    }
 }
